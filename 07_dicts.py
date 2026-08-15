9b70669759a47288bb5a8cfc58697a3f4f99c842
Thattus <saul_tato@hotmail.com>
2026-08-15 15:36:12 -0600

    print f para dicts, conditionals, loops, functions

--- 07_dicts.py
+++ 07_dicts.py
@@ -3,7 +3,7 @@
 my_dict = dict()  # Crear un diccionario vacío
 my_other_dict = {}  # Crear un diccionario vacío
 
-print(f"type(my_dict): {type(my_dict)}")  # Imprimir el tipo de my_dict, que es un diccionario
+print(f"\ntype(my_dict): {type(my_dict)}")  # Imprimir el tipo de my_dict, que es un diccionario
 print(f"type(my_other_dict): {type(my_other_dict)}")  # Imprimir el tipo de my_other_dict, que es
 
 my_other_dict = {
@@ -21,49 +21,50 @@ my_dict = {
     1: "one",  # Clave numérica
 }  # Crear un diccionario con un set como valor
 
-print(f"my_other_dict: {my_other_dict}")  # Imprimir el diccionario con diferentes tipos de datos
+print(f"\nmy_other_dict: {my_other_dict}")  # Imprimir el diccionario con diferentes tipos de datos
 print(f"my_dict: {my_dict}")  # Imprimir el diccionario con un set como valor
 
-print(f"my_dict['name']: {my_dict['name']}")  # Acceder al valor asociado a la clave "name" en my_dict
+print(f"\nmy_dict['name']: {my_dict['name']}")  # Acceder al valor asociado a la clave "name" en my_dict
 print(f"my_dict[1]: {my_dict[1]}")  # Acceder al valor asociado a la clave numérica 1 en my_dict
 
 my_dict["name"] = "Saul Zurita"  # Modificar el valor asociado a la clave "name" en my_dict
-print(f"my_dict['name']: {my_dict['name']}")  # Imprimir el valor modificado de la clave "name"
+print(f"\nmy_dict['name']: {my_dict['name']}")  # Imprimir el valor modificado de la clave "name"
 my_dict["calle"] = "Calle Falsa 123"  # Agregar una nueva clave "calle" con su valor en my_dict
 print(f"my_dict: {my_dict}")  # Imprimir el diccionario después de agregar la nueva clave
 
 my_dict["languages"].add("Java")  # Agregar un nuevo lenguaje al set asociado a la clave "languages"
-print(f"my_dict['languages']: {my_dict['languages']}")  # Imprimir el set de lenguajes después de agregar
+print(f"\nmy_dict['languages']: {my_dict['languages']}")  # Imprimir el set de lenguajes después de agregar
 del my_dict["languages"]  # Eliminar la clave "languages" del diccionario
 print(f"my_dict: {my_dict}")  # Imprimir el diccionario después de eliminar la clave "languages"
 
-print(f"Zurita in my_other_dict: {'Zurita' in my_other_dict}")  # Verificar si "Zurita" es un valor en el diccionario
+print(f"\nZurita in my_other_dict: {'Zurita' in my_other_dict}")  # Verificar si "Zurita" es un valor en el diccionario
 print(f"last_name in my_other_dict: {{'last_name' in my_other_dict}}")  # Verificar si "apellido" es una clave en el diccionario
 
-print(f"keys: {list(my_other_dict.keys())}")  # Imprimir las claves del diccionario
+print(f"\nkeys: {list(my_other_dict.keys())}")  # Imprimir las claves del diccionario
 print(f"values: {list(my_other_dict.values())}")  # Imprimir los valores del diccionario
 print(f"items: {list(my_other_dict.items())}")  # Imprimir los pares clave-valor del diccionario
 
-print(f"my_dict.fromkeys(['name', 'age'], 'unknown'): {my_dict.fromkeys(['name', 'age'], 'unknown')}")  # Crear un nuevo diccionario con claves y un valor por defecto
+print(f"\nmy_dict.fromkeys(['name', 'age'], 'unknown'): {my_dict.fromkeys(['name', 'age'], 'unknown')}")  # Crear un nuevo diccionario con claves y un valor por defecto
+print(f"dict.fromkeys(my_dict, ['Saul', 'Zurita']): {dict.fromkeys(my_dict, ['Saul', 'Zurita'])}")
 print(f"dict.fromkeys(['name', 'age'], 'unknown'): {dict.fromkeys(['name', 'age'], 'unknown')}")  # Crear un nuevo diccionario con claves y un valor por defecto usando dict
 print(f"my_other_dict.fromkeys(['name', 'age', 'Piso'], '7'): {my_other_dict.fromkeys(['name', 'age', 'Piso'], '7')}")  # Crear un nuevo diccionario con claves y un valor por defecto
 
 my_new_dict = my_other_dict.copy()  # Hacer una copia del diccionario my_other_dict
-print(f"my_new_dict: {my_new_dict}")  # Imprimir la copia del diccionario
+print(f"\nmy_new_dict: {my_new_dict}")  # Imprimir la copia del diccionario
 my_new_dict = dict.fromkeys(my_other_dict)  # Hacer una copia del diccionario my_other_dict usando dict.fromkeys
 print(f"my_new_dict: {my_new_dict}")  # Imprimir la copia del diccionario usando dict.fromkeys
 
-print(f"list(my_other_dict): {list(my_other_dict)}")  # Convertir las claves del diccionario en una lista e imprimirla
+print(f"\nlist(my_other_dict): {list(my_other_dict)}")  # Convertir las claves del diccionario en una lista e imprimirla
 print(f"list(my_other_dict.keys()): {list(my_other_dict.keys())}")  # Convertir las claves del diccionario en una lista
 print(f"list(my_other_dict.values()): {list(my_other_dict.values())}")  # Convertir los valores del diccionario en una lista e imprimirla
 print(f"list(my_other_dict.items()): {list(my_other_dict.items())}")  # Convertir los pares clave-valor del diccionario en una lista e imprimirla
 
-print(f"tuple(my_other_dict): {tuple(my_other_dict)}")  # Convertir las claves del diccionario en una tupla e imprimirla
+print(f"\ntuple(my_other_dict): {tuple(my_other_dict)}")  # Convertir las claves del diccionario en una tupla e imprimirla
 print(f"tuple(my_other_dict.keys()): {tuple(my_other_dict.keys())}")  # Convertir las claves del diccionario en una tupla
 print(f"tuple(my_other_dict.values()): {tuple(my_other_dict.values())}")  # Convertir los valores del diccionario en una tupla e imprimirla
 print(f"tuple(my_other_dict.items()): {tuple(my_other_dict.items())}")  # Convertir los pares clave-valor del diccionario en una tupla e imprimirla
 
-print(f"set(my_other_dict): {set(my_other_dict)}")  # Convertir las claves del diccionario en un set e imprimirlo
+print(f"\nset(my_other_dict): {set(my_other_dict)}")  # Convertir las claves del diccionario en un set e imprimirlo
 print(f"set(my_other_dict.keys()): {set(my_other_dict.keys())}")  # Convertir las claves del diccionario en un set
 print(f"set(my_other_dict.values()): {set(my_other_dict.values())}")  # Convertir los valores del diccionario en un set e imprimirlo
 print(f"set(my_other_dict.items()): {set(my_other_dict.items())}")  # Convertir los pares clave-valor del diccionario en un set e imprimirlo
--- 09_loops.py
+++ 09_loops.py
@@ -3,25 +3,25 @@
 # while loop
 i = 0  # Inicializar i en 0
 while i < 10:  # Mientras i sea menor que 10
-    print("while", i)  # Imprimir el valor de i en cada iteración
-    i += 2  # Incrementar i en 2\
+    print(f"while i: {i}")  # Imprimir el valor de i en cada iteración
+    i += 2  # Incrementar i en 2    
 else:
-    print("Fin del while 1")
+    print(f"Fin del while 1\n")
 
 # for loop
 for i in range(0,10,2):  # Iterar desde 0 hasta 10 con un paso de 2
-    print("for", i)  # Imprimir el valor de i en cada iteración
+    print(f"for i: {i}")  # Imprimir el valor de i en cada iteración
 else:
-    print("Fin del for 1")
+    print(f"Fin del for 1\n")
 
 # break statement
 for i in range(10):
     if i == 5:  # Si i es igual a 5
-        print("For break in:", i)
+        print(f"For break in: {i}")  # Imprimir el valor de i antes de romper el bucle  
         break  # Romper el bucle
-    print("For break in 5", i)  # Imprimir el valor de i en cada iteración
+    print(f"For break in 5: {i}")  # Imprimir el valor de i en cada iteración
 else:
-    print("Fin del for break 1")
+    print("Fin del for 2 break 1")
 
 my_list = [1, 2, 3, 4, 5]  # Lista de números
 my_tuple = (1, 2, 3, 4, 5)  # Tupla de números
@@ -30,44 +30,44 @@ my_dict = {"Nombre": "Saul", "Apellido": "Zurita", "Edad": 36}  # Diccionario de
 
 # Iterar sobre una lista
 for i in my_list:  # Iterar sobre cada elemento de la lista
-    print("For in list:", i)  # Imprimir el valor de i en cada iteración
-
+    print(f"For in list: {i}")  # Imprimir el valor de i en cada iteración
+print(f"fin de for 3")
 # Iterar sobre una tupla
 for i in my_tuple:  # Iterar sobre cada elemento de la tupla
-    print("For in tuple:", i)  # Imprimir el valor de i en cada iteración
+    print(f"For in tuple: {i}")  # Imprimir el valor de i en cada iteración
 
 # Iterar sobre un conjunto
 for i in my_set:  # Iterar sobre cada elemento del conjunto 
-    print("For in set:", i)  # Imprimir el valor de i en cada iteración
+    print(f"For in set: {i}")  # Imprimir el valor de i en cada iteración
 
 # Iterar sobre un diccionario
 for key, value in my_dict.items():  # Iterar sobre cada clave y valor del
-    print("For in dict:", key, value)  # Imprimir la clave y el valor en cada iteración
+    print(f"For in dict: {key}, {value}")  # Imprimir la clave y el valor en cada iteración
 
 # Iterar sobre las claves de un diccionario
 for key in my_dict:  # Iterar sobre cada clave del diccionario  
-    print("For in dict keys:", key)  # Imprimir la clave en cada iteración
+    print(f"For in dict keys: {key}")  # Imprimir la clave en cada iteración
     if key == "Edad":
-        print("For in dict keys:", my_dict[key])
+        print(f"For in dict keys: {my_dict[key]}")  # Imprimir el valor asociado a la clave "Edad"
 else:
-    print("Fin del for in dict keys")
+    print(f"Fin del for in dict keys")
 
 # Iterar sobre los valores de un diccionario
 for value in my_dict.values():  # Iterar sobre cada valor del diccionario
-    print("For in dict values:", value)  # Imprimir el valor en cada iteración
+    print(f"For in dict values: {value}")  # Imprimir el valor en cada iteración
     if value == 36:
-        print("For in dict values:", value)
+        print(f"For in dict values: {value}")  # Imprimir el valor si es igual a 36 
         break
 else:
-    print("Fin del for in dict values")
+    print(f"Fin del for in dict values")
 
 
 # Iterar sobre los valores de un diccionario
 for value in my_dict.values():  # Iterar sobre cada valor del diccionario
-    print("For_last in dict values:", value)  # Imprimir el valor en cada iteración
+    print(f"For_last in dict values: {value}")  # Imprimir el valor en cada iteración
     if value == "Zurita":  # Si el valor es "Zurita"
-        print("For in dict values:", value)
+        print(f"For in dict values: {value}"    )  # Imprimir el valor si es igual a "Zurita" 
         continue  # Continuar con la siguiente iteración si el valor es 36
-    print("Continuando con el siguiente valor:", value)  # Imprimir si se continúa
+    print(f"Continuando con el siguiente valor: {value}")  # Imprimir si se continúa
 else:
-    print("Fin del for in dict values")
+    print(f"Fin del for in dict values")
